67c5ee6b9db96025f61c0a4ed8ed62daee39c2a7
Cauê Thenório <caue@thenorio.com.br>
2023-10-27 18:05:38 -0300

    Updated vite config to generate relative hrefs in the index html file

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { resolve } from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   resolve: {
     alias: {
       src: resolve(__dirname, './src'),
